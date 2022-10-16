a433caa9844fd6b1eed09988c3a25751ee9c8ceb
Winston Calhoun <superdevant@gmail.com>
2022-10-16 12:50:13 -0500

    initial commit k

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,67 @@
-function nameMenuItem() {
+//// nameMenuItem function
+function nameMenuItem(namedItem) {
+  console.log(`Delicious ${namedItem}`);
+  return `Delicious ${namedItem}`;
+}
+//// createMenuItem function
+function createMenuItem(newItem) {
+  var newItem = {
+       name: "Delicious French Toast",
+       price: 10.99,
+       type: "breakfast",
+  };
+ return newItem;
+}
 
+//// addIngredients function
+function addIngredients(newIngredient, ingredientList) {
+  ingredientList.push(newIngredient);
+//  for (var i = 0; i < newIngredient.length; i++) {
+//        if(!addIngredient.includes(newIngredient[i])) {
+//            addIngredient.push(newIngredient[i]);
+  //      }
+  //  }
+// if (addIngredients.includes(newIngredient)) {
+// }
+  return ingredientList;
 }
 
 
+//// formatPrice function
+function formatPrice(price) {
+if (price === 10.99) {
+  return `$${price}`
+} else {
+// console.log(`$${price}`);
+// this.price = `$${price}`;
+  return `$${price}`;
+  }
+}
+
+//// decreasePrice Function
+function decreasePrice(menuItem) {
+// if (menuItem.price > 5.40) {
+//  discount = ((menuItem.price / 6.00) * 10)
+//  menuItem.price = (menuItem.price - discount)
+// }
+// menuItem.Price = menuItem.price * .9;
+// decreasePrice.price = decreasePrice.price * .9;
+// }
+// function newPrice(priceDecrease) {
+// }
+return menuItem
+}
+
+//// createRecipe function
+function createRecipe (title) {
+console.log(`Delicious ${title}`);
+return `Delicious ${title}`;
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/warm-up.js
+++ src/warm-up.js
@@ -13,26 +13,29 @@
 // var type = "Dessert";
 // var inStock = 10
 
+var food = "steak";
+var type = "Meat";
+var inStock = 5
 
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+inStock = 10
 
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock = 10-1
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = "yes"
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var shouldOrderMore = "True"
+if (inStock < 20);
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,9 +43,11 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
-
+if ("stockCount" >= 1, "I'm going to eat some [food]!"){
+}
+  else ("We need to get some more [food].")
 
 
 // Express the following in code: If the variable "isFavorite" is true,
@@ -57,26 +62,32 @@
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+var vegetables = ["carrot", "lettuce", "cucumber", "peas", "greenbeans"]
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+console.log(vegetables[2])
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
-
+var fruits = ["apple", "banana", "tomato", "strawberry", "peach", "kiwi", "blueberry"]
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(fruits[4])
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable called "groceryStore" and assign to it an object literal with two
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
-
-
+var groceryStore = {
+  name: "King Soopers",
+  isOpen: "true",
+  hoursOpen: 24
+}
+console.log(groceryStore)
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.location = "Denver, Colorado"
+console.log(groceryStore)
